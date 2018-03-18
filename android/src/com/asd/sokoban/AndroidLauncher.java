42b2a7caea9f4a2853b19cf9ca36201760675e84
fabii <popadianafabiola@yahoo.com>
2018-03-18 18:18:35 +0200

    first commit

---
+++ android/src/com/asd/sokoban/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.asd.sokoban;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.asd.sokoban.SokobanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SokobanGame(), config);
+	}
+}
---
+++ desktop/src/com/asd/sokoban/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.asd.sokoban.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asd.sokoban.SokobanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 840;
+		config.width = 480;
+		new LwjglApplication(new SokobanGame(), config);
+	}
+}
